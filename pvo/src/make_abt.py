f0712960b121e694b55199326d38986acd2c391d
myloansk <cosmyl77@gmail.com>
2022-05-12 08:43:45 +0300

    Rename interface IDataPrep to Source and turn method's names to snake case

---
+++ pvo/src/make_abt.py
@@ -0,0 +1,13 @@
+from skeleton_abt import Source 
+
+class Coolers(Source):
+    def load_data(self)->DataFrame:pass 
+
+    def filter_data(self)->DataFrame:pass 
+
+    def feature_engineering(self) -> DataFrame:pass 
+
+    def impute_nans(self) -> DataFrame:pass 
+
+    
+        
