8da6da77ff714e2432fe44e44aa940ae10cc387e
myloansk <cosmyl77@gmail.com>
2022-05-17 15:19:44 +0300

    Adding commnet to Coolers class

--- pvo/src/make_abt.py
+++ pvo/src/make_abt.py
@@ -21,33 +21,49 @@ spark.sql("set spark.sql.execution.arrow.pyspark.fallback.enabled=false")
 
 class Cooler(Source):
     """
-    _summary_
+    Concrete class in which all abstract stages of Source are implemented 
+    with respect to coolers data
 
-    _extended_summary_
-
-    :param Source: _description_
-    :type Source: _type_
+    :param Source:  parent class used as an interface in which
+                    a template method that contains a skeleton of some algorithm 
+                    composed of calls, usually to abstract primitive operations. 
+    :type Source: class
     """
     def __init__(self, config):
         self.cooleDf:DataFrame = None
         self.this_config = config 
 
     def load_data(self)->None:
+        """
+        Loads cooler data using config Dict for getting the filepath
+        """
         dfs = []
         for companyCode in self.this_config['company_code']:
             dfs.append(spark.read.option("header", "true").parquet(self.this_config['cooler_file_path'].format(CAPS_CC = self.this_config['country_code'].upper(), companyCode = companyCode )))
         self.coolerDf = reduce(DataFrame.unionAll, dfs)
 
     def filter_data(self)->None:
+        """
+        Filter cooler data using knowledge transferred by stakeholder/business units
+
+        """
         self.coolerDf = self.coolerDf.selectExpr([colExpr for colExpr in self.this_config['selected_coolers_column_list']])\
                                     .filter(self.this_config['filtering_cooler_conditions'])
         # --- Remove duplicates (if exist) ---
         self.coolerDf = self.coolerDf.select(*self.this_config['output_selected_column_list']).distinct()
         
     def impute_nans(self)->None:
+        """
+        Impute Nans on the derived columns using 0 as agreed with business stakeholders
+
+        """
         self.coolerDf = self.coolerDf.fillna(0, ['COOLER_DOORS_SUM', 'COOLER_EQUIP_COUNT'])
     
     def feature_engineering(self)->None:
+        """
+        Create features associated with coolers data 
+        that will take part in estimating potential of each outlet 
+        """
         # --- Create features ---
         self.coolerDf = self.coolerDf.groupBy('CUSTOMER').agg(f.sum('ACTUAL_DOORS').alias('COOLER_DOORS_SUM'), f.countDistinct('EQUIPMENT').alias('COOLER_EQUIP_COUNT'))
         
